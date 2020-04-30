bbf5cf1867a3de217f68f75904122ff9487c773c
Abelardo Ramírez Fuente <abelardo-0@hotmail.com>
2020-04-30 16:03:15 -0500

    modal to delete comments have been added

--- resources/views/videos/comments.blade.php
+++ resources/views/videos/comments.blade.php
@@ -25,7 +25,30 @@ Comments
                         Uploaded by <strong>{{$comment->user->name}} {{$comment->user->surname}}</strong> {{FormatTime::LongTimeFilter($comment->created_at)}}
                     </div>
                     <div class="card-body">
-                        {{$comment->body}}
+                            {{$comment->body}}
+                            @if(Auth::check() && (Auth::user()->id == $comment->user_id || Auth::user()->id == $video->user_id))
+                                <div class="pull-right">
+                                    <a href="#deleteModal{{$comment->id}}" role="button" class="btn btn-primary" data-toggle="modal">Delete</a>
+                                </div>
+                                <div id="deleteModal{{$comment->id}}" class="modal fade">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h4 class="modal-title">¿Estás seguro?</h4>
+                                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <p>¿Are you sure you want to delete this comment?</p>
+                                                <p>{{$comment->body}}</p>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                                                <button type="button" class="btn btn-danger">Delete</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endif
                     </div>
                 </div>
             </div>
@@ -33,3 +56,8 @@ Comments
         @endforeach
     </div>
 @endif
+
+<!-- Botón en HTML (lanza el modal en Bootstrap) -->
+  
+<!-- Modal / Ventana / Overlay en HTML -->
+
